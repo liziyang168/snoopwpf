76d3d78bddf807bceecaefd332b777be75e0e7d1
Bastian Schmidt <batzen@gmx.org>
2026-02-22 11:25:48 +0100

    Improving formatting and nullable

--- Snoop.Core/Infrastructure/TreeExporter.cs
+++ Snoop.Core/Infrastructure/TreeExporter.cs
@@ -2,13 +2,13 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Windows;
+using System.Windows.Media;
 using System.Xml;
 using Snoop.Data.Tree;
+using Snoop.Infrastructure.Helpers;
 
 public static class TreeExporter
 {
@@ -44,42 +44,50 @@ public class ExportOptions : BaseNotifyObject
 {
     public bool UseFilter
     {
-        get; set => this.Set(ref field, value);
+        get;
+        set => this.Set(ref field, value);
     }
 
     public TreeItem? TreeItem
     {
-        get; set => this.Set(ref field, value);
+        get;
+        set => this.Set(ref field, value);
     }
 
     public bool Recurse
     {
-        get; set => this.Set(ref field, value);
+        get;
+        set => this.Set(ref field, value);
     }
 
     public bool ExportXamlStyle
     {
-        get; set => this.Set(ref field, value);
+        get;
+        set => this.Set(ref field, value);
     } = true;
 
     public bool IncludeDefaultEmptyValues
     {
-        get; set => this.Set(ref field, value);
+        get;
+        set => this.Set(ref field, value);
     } = false;
 
     public bool IncludeTypenameOnlyValues
     {
-        get; set => this.Set(ref field, value);
+        get;
+        set => this.Set(ref field, value);
     } = false;
 
     public bool IncludeSystemCollectionNamespaceValues
     {
-        get; set => this.Set(ref field, value);
+        get;
+        set => this.Set(ref field, value);
     } = false;
 
     public bool RoundDecimals
     {
-        get; set => this.Set(ref field, value);
+        get;
+        set => this.Set(ref field, value);
     } = true;
 }
 #pragma warning restore SA1513 // Closing brace should be followed by blank line
@@ -87,30 +95,22 @@ public class ExportOptions : BaseNotifyObject
 
 public class XMLTreeExporter
 {
-    private static bool IsSimpleType(Type type) => type.IsPrimitive || type == typeof(decimal) || type == typeof(DateTime) || type == typeof(DateTimeOffset) || type == typeof(TimeSpan) || type == typeof(Guid);
-
     private static readonly Dictionary<Type, object?> defaultValueCache = new();
+
+    private static bool IsSimpleType(Type type)
+    {
+        return type.IsPrimitive || type == typeof(decimal) || type == typeof(DateTime) || type == typeof(DateTimeOffset) || type == typeof(TimeSpan) || type == typeof(Guid);
+    }
 
     public void Export(TreeItem treeItem, TextWriter textWriter, PropertyFilter? filter, bool recurse = true)
     {
-        var options = new ExportOptions
-        {
-            Recurse = recurse,
-            ExportXamlStyle = true,
-            RoundDecimals = true,
-            IncludeDefaultEmptyValues = false
-        };
+        var options = new ExportOptions { Recurse = recurse, ExportXamlStyle = true, RoundDecimals = true, IncludeDefaultEmptyValues = false };
         this.Export(treeItem, textWriter, filter, options);
     }
 
     public void Export(TreeItem treeItem, TextWriter textWriter, PropertyFilter? filter, ExportOptions options)
     {
-        var writerSettings = new XmlWriterSettings
-        {
-            Encoding = textWriter.Encoding,
-            Indent = true,
-            NewLineOnAttributes = false
-        };
+        var writerSettings = new XmlWriterSettings { Encoding = textWriter.Encoding, Indent = true, NewLineOnAttributes = false };
 
         using var xmlWriter = XmlWriter.Create(textWriter, writerSettings);
         xmlWriter.WriteStartDocument(true);
@@ -120,12 +120,12 @@ public class XMLTreeExporter
 
     private void ExportItem(TreeItem treeItem, XmlWriter xmlWriter, PropertyFilter? filter, ExportOptions options)
     {
-        if (!options.ExportXamlStyle)
-    {
-        xmlWriter.WriteStartElement("node");
-        xmlWriter.WriteAttributeString("name", treeItem.Name);
-        xmlWriter.WriteAttributeString("displayName", treeItem.DisplayName);
-        xmlWriter.WriteAttributeString("targetType", treeItem.TargetType.FullName!);
+        if (options.ExportXamlStyle is false)
+        {
+            xmlWriter.WriteStartElement("node");
+            xmlWriter.WriteAttributeString("name", treeItem.Name);
+            xmlWriter.WriteAttributeString("displayName", treeItem.DisplayName);
+            xmlWriter.WriteAttributeString("targetType", treeItem.TargetType.FullName!);
         }
         else
         {
@@ -135,112 +135,119 @@ public class XMLTreeExporter
 
         var propertyInformations = PropertyInformation.GetProperties(treeItem.Target);
 
-        if (propertyInformations.Any())
+        if (propertyInformations.Any() is false)
         {
-            if (!options.ExportXamlStyle)
+            xmlWriter.WriteEndElement();
+            return;
+        }
+
+        if (options.ExportXamlStyle is false)
         {
             xmlWriter.WriteStartElement("properties");
+        }
+
+        foreach (var propertyInformation in propertyInformations)
+        {
+            if (filter is not null
+                && filter.ShouldShow(propertyInformation) == false)
+            {
+                continue;
             }
 
-            foreach (var propertyInformation in propertyInformations)
+            var rawVal = propertyInformation.Value;
+            if (options.IncludeDefaultEmptyValues is false)
             {
-                if (filter is not null
-                    && filter.ShouldShow(propertyInformation) == false)
+                var skipValue = false;
+                skipValue |= rawVal is null;
+                skipValue |= rawVal is string strVal && string.IsNullOrEmpty(strVal);
+
+                if (skipValue is false
+                    && IsSimpleType(propertyInformation.PropertyType.Type))
                 {
+                    if (defaultValueCache.TryGetValue(propertyInformation.PropertyType.Type, out var defaultValue) is false)
+                    {
+                        defaultValueCache[propertyInformation.PropertyType.Type] = defaultValue = Activator.CreateInstance(propertyInformation.PropertyType.Type);
+                    }
+
+                    // rawVal can't be null if skipValue is true
+                    if (rawVal!.Equals(defaultValue))
+                    {
+                        skipValue = true;
+                    }
+                }
+
+                if (skipValue is false
+                    && options.IncludeSystemCollectionNamespaceValues is false)
+                {
+                    var typeNamespace = propertyInformation.PropertyType.Type.Namespace ?? string.Empty;
+                    if (typeNamespace.StartsWith("System.Collections", StringComparison.Ordinal))
+                    {
+                        skipValue = true;
+                    }
+                }
+
+                if (skipValue)
+                {
+                    propertyInformation.Teardown();
                     continue;
                 }
-
-                var rawVal = propertyInformation.Value;
-                if (options.IncludeDefaultEmptyValues == false)
-                {
-                    var skipValue = false;
-                    skipValue |= rawVal is null;
-                    skipValue |= rawVal is string strVal && string.IsNullOrEmpty(strVal);
-                    if (!skipValue && IsSimpleType(propertyInformation.PropertyType.Type))
-                    {
-                        if (!defaultValueCache.TryGetValue(propertyInformation.PropertyType.Type, out var defaultValue))
-                        {
-                            defaultValueCache[propertyInformation.PropertyType.Type] = defaultValue = Activator.CreateInstance(propertyInformation.PropertyType.Type);
-                        }
-
-                        if (rawVal!.Equals(defaultValue))
-                        {
-                            skipValue = true;
-                        }
-                    }
-
-                    if (!skipValue && !options.IncludeSystemCollectionNamespaceValues)
-                    {
-                        var typeNamespace = propertyInformation.PropertyType.Type.Namespace ?? string.Empty;
-                        if (typeNamespace.StartsWith("System.Collections", StringComparison.Ordinal))
-                        {
-                            skipValue = true;
-                        }
-                    }
-
-                    if (skipValue)
-                    {
-                        propertyInformation.Teardown();
-                        continue;
-                    }
-                }
-
-                if (rawVal == null)
-                {
-                    rawVal = "null";
-                }
-
-                string? value;
-                if (options.RoundDecimals)
-                {
-                    value = rawVal switch
-                    {
-                        double d => d.ToString("0.#"),
-                        float f => f.ToString("0.#"),
-                        decimal m => m.ToString("0.#"),
-                        Size sz => $"{sz.Width:0.#}x{sz.Height:0.#}",
-                        Point pt => $"({pt.X:0.#},{pt.Y:0.#})",
-                        Thickness thick => $"{(thick.Left == thick.Right && thick.Right == thick.Top && thick.Top == thick.Bottom ? thick.Left.ToString("0.#") : $"{thick.Left:0.#},{thick.Top:0.#},{thick.Right:0.#},{thick.Bottom:0.#}")}",
-                        System.Windows.Media.Geometry geo => $"{geo?.GetOutlinedPathGeometry()?.Figures?.Count} Geo Figures",
-                        _ => rawVal.ToString()
-                    };
-                }
-                else
-                {
-                    value = rawVal.ToString()!;
-                }
-
-                if (options.IncludeTypenameOnlyValues == false)
-                {
-#pragma warning disable CA1307 // Specify StringComparison for clarity
-                    if (value!.Contains('.') && value == propertyInformation.PropertyType.Type.FullName)
-                    {
-                        propertyInformation.Teardown();
-                        continue;
-                    }
-#pragma warning restore CA1307 // Specify StringComparison for clarity
-                }
-
-                if (!options.ExportXamlStyle)
-                {
-                xmlWriter.WriteStartElement("property");
-                xmlWriter.WriteAttributeString("displayName", propertyInformation.DisplayName);
-                    xmlWriter.WriteAttributeString("value", value);
-                xmlWriter.WriteEndElement();
-                }
-                else
-                {
-                    var attributeName = GetValidXmlName(propertyInformation.DisplayName);
-                    xmlWriter.WriteAttributeString(attributeName, value);
-                }
-
-                propertyInformation.Teardown();
             }
 
-            if (!options.ExportXamlStyle)
+            rawVal ??= "null";
+
+            string? value;
+            if (options.RoundDecimals)
             {
-            xmlWriter.WriteEndElement();
+                value = rawVal switch
+                {
+                    double d => d.ToString("0.#"),
+                    float f => f.ToString("0.#"),
+                    decimal m => m.ToString("0.#"),
+                    Size sz => $"{sz.Width:0.#}x{sz.Height:0.#}",
+                    Point pt => $"({pt.X:0.#},{pt.Y:0.#})",
+                    Thickness thick => thick.Left.AreClose(thick.Right) && thick.Right.AreClose(thick.Top) && thick.Top.AreClose(thick.Bottom)
+                        ? thick.Left.ToString("0.#") // all values were the same
+                        : $"{thick.Left:0.#},{thick.Top:0.#},{thick.Right:0.#},{thick.Bottom:0.#}",
+                    Geometry geo => $"{geo.GetOutlinedPathGeometry().Figures?.Count} Geo Figures",
+                    var _ => rawVal.ToString()
+                };
+            }
+            else
+            {
+                value = rawVal.ToString();
+            }
+
+            if (options.IncludeTypenameOnlyValues is false)
+            {
+#pragma warning disable CA1307 // Specify StringComparison for clarity
+                if (value?.Contains('.') is true
+                    && value == propertyInformation.PropertyType.Type.FullName)
+                {
+                    propertyInformation.Teardown();
+                    continue;
+                }
+#pragma warning restore CA1307 // Specify StringComparison for clarity
+            }
+
+            if (options.ExportXamlStyle is false)
+            {
+                xmlWriter.WriteStartElement("property");
+                xmlWriter.WriteAttributeString("displayName", propertyInformation.DisplayName);
+                xmlWriter.WriteAttributeString("value", value);
+                xmlWriter.WriteEndElement();
+            }
+            else
+            {
+                var attributeName = GetValidXmlName(propertyInformation.DisplayName);
+                xmlWriter.WriteAttributeString(attributeName, value);
+            }
+
+            propertyInformation.Teardown();
         }
+
+        if (options.ExportXamlStyle is false)
+        {
+            xmlWriter.WriteEndElement();
         }
 
         if (options.Recurse)
@@ -261,6 +268,6 @@ public class XMLTreeExporter
             return "Element";
         }
 
-        return XmlConvert.EncodeLocalName(name);
+        return XmlConvert.EncodeLocalName(name)!; // can't be null as name can't be null
     }
 }
